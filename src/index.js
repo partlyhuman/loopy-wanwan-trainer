5dde39d540434852928070c2a61ff710f4caefbe
Roger Braunstein <roger@oooh.tv>
2023-10-17 22:49:25 -0700

    Working trainer but clear game data seems to be tied to name

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import Encoding from 'encoding-japanese';
 const $ = document.querySelector.bind(document);
 
 /*
@@ -18,15 +19,23 @@ FORMAT
                      (save data, starting with 0x00)
                      (just started = 000...0D32)
                      (just started = 000000000000000000000000000000000000000000000000000000000C32)
-                     (clear game = 020000000000000000000000000000000000000000000000000000000DAF)
+                     (clear game =   0C00 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0E26)
 
 70-af 64 bytes | save #2
 b0-ef 64 bytes | save #3
 
 */
 
-const SAVE_BYTES = 64;
-const SAVE_HEADER_WORDS = [0x5B57, 0x414E, 0x2057, 0x414E, 0x2053, 0x544F, 0x5259, 0x215D];
+const SAVE_FILE_BYTES = 0x2000;
+const SINGLE_SAVE_BYTES = 0x40;
+const SAVE_HEADER_BYTES = 0x30;
+const NAME_BYTES = 9;
+const SAVE_MARKER_U8 = new Uint8Array([0x5B, 0x57, 0x41, 0x4E, 0x20, 0x57, 0x41, 0x4E, 0x20, 0x53, 0x54, 0x4F, 0x52, 0x59, 0x21, 0x5D]);
+
+const STATE_CLEARED_U8 = new Uint8Array(30);
+STATE_CLEARED_U8[0] = 0x0C;
+STATE_CLEARED_U8[28] = 0x0E;
+STATE_CLEARED_U8[29] = 0x26;
 
 function saveBufferToFile(buffer, filename = 'wanwan.sav') {
     const a = document.createElement('a');
@@ -35,18 +44,54 @@ function saveBufferToFile(buffer, filename = 'wanwan.sav') {
     a.click();
 }
 
-$('#go').addEventListener('click', () => {
-    const saveBuffer = new ArrayBuffer(SAVE_BYTES);
-    const save = new Uint8Array(saveBuffer);
-    const view = new DataView(saveBuffer);
+function getNameBytes(saveNum, nameClass) {
+    const nameU8 = new Uint8Array(NAME_BYTES);
+    // nameU8.fill(0xff); // makes any difference?
+    const $name = $(`.save${saveNum} input.${nameClass}`);
+    let name = $name.value;
 
-    // add header
-    for (let i = 0; i < SAVE_HEADER_WORDS.length; i++) {
-        console.log(i);
-        view.setUint16(i * 2, SAVE_HEADER_WORDS[i]);
+    if (Encoding.detect(name) === 'ASCII') {
+        name = Encoding.toZenkakuCase(name);
+        $name.value = name;
     }
 
-    saveBufferToFile(save);
+    const nameUnicode = Encoding.stringToCode($name.value);
+    const nameSJIS = Encoding.convert(nameUnicode, {from: 'UNICODE', to: 'SJIS'});
+    nameU8.set(nameSJIS, 0);
+    nameU8.set([0x00], Math.min(nameSJIS.length, nameU8.length - 1));
+    // console.log($name.value, nameUnicode.map(x => x.toString(16)), nameSJIS.map(x => x.toString(16)), nameU8);
+    return nameU8;
+}
+
+$('#go').addEventListener('click', () => {
+    const saveBuffer = new ArrayBuffer(SAVE_FILE_BYTES);
+    const saveBufferU8 = new Uint8Array(saveBuffer);
+    let pos = 0x0;
+
+    // 48-byte file header
+    saveBufferU8.fill(0xff, 0, SAVE_HEADER_BYTES);
+    saveBufferU8.set(SAVE_MARKER_U8, 0);
+    pos = SAVE_HEADER_BYTES;
+
+    for (let saveNum = 0; saveNum < 3; saveNum++) {
+        // header
+        saveBufferU8.set(SAVE_MARKER_U8, pos);
+        pos += SAVE_MARKER_U8.length;
+
+        // human name
+        saveBufferU8.set(getNameBytes(saveNum, 'you'), pos);
+        pos += NAME_BYTES;
+
+        // dog name
+        saveBufferU8.set(getNameBytes(saveNum, 'dog'), pos);
+        pos += NAME_BYTES;
+
+        // clear game
+        saveBufferU8.set(STATE_CLEARED_U8, pos);
+        pos += STATE_CLEARED_U8.length;
+    }
+
+    saveBufferToFile(saveBuffer);
 });
 
 
