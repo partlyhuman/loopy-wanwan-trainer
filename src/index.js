f5b42268d202084526541176f777fc8320fc19c4
Roger Braunstein <roger@oooh.tv>
2023-10-18 10:17:59 -0700

    Consider it trained

--- src/index.js
+++ src/index.js
@@ -6,20 +6,18 @@ FORMAT
 00-2f 48 bytes | file header
     00-0f 16 bytes | [WAN WAN STORY!]
                      (first byte might be set to 0 or this might be an error?)
-    10-2f 64 bytes | <padded with 0xff>
-
-(possibly delete first byte to 0?)
+    10-2f 32 bytes | <padded with 0xff>
 
 30-6f 64 bytes | a single save slot
     30-3f 16 bytes | [WAN WAN STORY!]
     40-47  9 bytes | <human name in SJIS, 2 bytes per character, up to 4 characters, terminated by 00>
     49-51  9 bytes | <dog name in SJIS, 2 bytes per character, up to 4 characters, terminated by 00>
                      (if not yet set, "？？？？" is used, fullwidth characters, 0x81 0x48 x 4)
-    52-6f 30 bytes |
-                     (save data, starting with 0x00)
-                     (just started = 000...0D32)
-                     (just started = 000000000000000000000000000000000000000000000000000000000C32)
-                     (clear game =   0C00 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0000 0E26)
+    52-6b 26 bytes |
+                     (game state, initialized to 0s)
+                     (clear game's first byte is 0C)
+    6c-6f  4 bytes | <16-bit sum of all 60 single bytes in this save slot up until here (0x30-0x6b)>
+                     (possibly 2 bytes, max possible value 3D C2 or 4 bytes max possible 00 00 3C C3, either way only last byte is significant)
 
 70-af 64 bytes | save #2
 b0-ef 64 bytes | save #3
@@ -34,8 +32,6 @@ const SAVE_MARKER_U8 = new Uint8Array([0x5B, 0x57, 0x41, 0x4E, 0x20, 0x57, 0x41,
 
 const STATE_CLEARED_U8 = new Uint8Array(30);
 STATE_CLEARED_U8[0] = 0x0C;
-STATE_CLEARED_U8[28] = 0x0E;
-STATE_CLEARED_U8[29] = 0x26;
 
 function saveBufferToFile(buffer, filename = 'wanwan.sav') {
     const a = document.createElement('a');
@@ -63,32 +59,45 @@ function getNameBytes(saveNum, nameClass) {
     return nameU8;
 }
 
+function sumBytes(bufferU8, start, end) {
+    // no overflow check necessary
+    let sum = 0x0;
+    for (let i = start; i < end; i++) {
+        sum += bufferU8[i];
+    }
+    return sum;
+}
+
 $('#go').addEventListener('click', () => {
     const saveBuffer = new ArrayBuffer(SAVE_FILE_BYTES);
     const saveBufferU8 = new Uint8Array(saveBuffer);
-    let pos = 0x0;
 
     // 48-byte file header
     saveBufferU8.fill(0xff, 0, SAVE_HEADER_BYTES);
     saveBufferU8.set(SAVE_MARKER_U8, 0);
-    pos = SAVE_HEADER_BYTES;
+
+    const [M, N, S] = [SAVE_MARKER_U8.byteLength, NAME_BYTES, STATE_CLEARED_U8.byteLength];
+    console.assert(SINGLE_SAVE_BYTES === M + 2*N + S);
 
     for (let saveNum = 0; saveNum < 3; saveNum++) {
+        const pos = SAVE_HEADER_BYTES + saveNum * SINGLE_SAVE_BYTES;
+
         // header
         saveBufferU8.set(SAVE_MARKER_U8, pos);
-        pos += SAVE_MARKER_U8.length;
 
         // human name
-        saveBufferU8.set(getNameBytes(saveNum, 'you'), pos);
-        pos += NAME_BYTES;
+        saveBufferU8.set(getNameBytes(saveNum, 'you'), pos + M);
 
         // dog name
-        saveBufferU8.set(getNameBytes(saveNum, 'dog'), pos);
-        pos += NAME_BYTES;
+        saveBufferU8.set(getNameBytes(saveNum, 'dog'), pos + M + N);
 
         // clear game
-        saveBufferU8.set(STATE_CLEARED_U8, pos);
-        pos += STATE_CLEARED_U8.length;
+        saveBufferU8.set(STATE_CLEARED_U8, pos + M + N + N);
+
+        // checksum
+        const sum = sumBytes(saveBufferU8, pos, pos + SINGLE_SAVE_BYTES - 2);
+        saveBufferU8[pos + SINGLE_SAVE_BYTES - 2] = (sum >> 8) & 0xff;
+        saveBufferU8[pos + SINGLE_SAVE_BYTES - 1] = sum & 0xff;
     }
 
     saveBufferToFile(saveBuffer);
